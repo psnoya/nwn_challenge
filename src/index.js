27d89a293c7946fc5516ca830505b500c9d6e241
psnoya <paulina.s.noya@gmail.com>
2021-01-03 12:18:19 +0100

    importing th API

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import 'semantic-ui-css/semantic.min.css'
 import App from './App'
 import { Provider } from 'react-redux'
 import configureStore from './state/store/configureStore'
+import axios from 'axios'
 
+axios.defaults.baseURL = "https://newsapi.org/v2"
 const store = configureStore()
 window.store = store
 
